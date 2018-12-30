e8c4d64bf4585b0365b0ff6cd2c76fc0c3cbe447
Senthilnathan M <senthilnathan.maadasamy@gmail.com>
2018-12-30 23:14:57 +0530

    Committed migrations

--- employees/models.py
+++ employees/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    is_company_admin = models.BooleanField(default=False)
-
     name = models.CharField(max_length=100)
-    #joining_date = models.DateTimeField(default=datetime.now)
-    #address = models.CharField(max_length=200)    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_company_admin = models.BooleanField(default=False)
+    team = models.ForeignKey('Team', on_delete=models.SET_NULL, blank=True, null=True)
 
-#class CompanyAdmin(models.Model):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    #joining_date = models.DateTimeField(default=datetime.now)
+    #address = models.CharField(max_length=200)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
